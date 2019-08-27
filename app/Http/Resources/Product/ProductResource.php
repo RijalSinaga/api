66af0cc86bb5557c735ccc7aa0c5e9187db876bd
rijalsinaga <rsrizalsinaga@gmail.com>
2019-08-27 22:06:41 +0700

    edit model resource and project model

---
+++ app/Http/Resources/Product/ProductResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources\Product;
+
+// use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Http\Resources\Json\Resource;
+
+// class ProductResource extends JsonResource
+class ProductResource extends Resource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'name' => $this->name,
+            'description' => $this->detail,
+            'price' => $this->price,
+            'stock' => $this->stock,
+            'discount' => $this->discount
+        ];
+    }
+}
