1c13cd9610178560d1b2d0a4c72ec906715aa707
rijalsinaga <rsrizalsinaga@gmail.com>
2019-08-27 21:06:55 +0700

    database model seeder and insert database to DB folder

--- database/migrations/2019_08_27_112017_create_reviews_table.php
+++ database/migrations/2019_08_27_112017_create_reviews_table.php
@@ -15,8 +15,8 @@ class CreateReviewsTable extends Migration
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->Increments('id');
-            $table->integer('produt_id')->unsigned()->index();
-            $table->foreign('produt_id')->references('id')->on('products')->onDelete('cascade');
+            $table->integer('product_id')->unsigned()->index();
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
             $table->string('customer');
             $table->text('review');
             $table->integer('star');
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        factory(App\Model\Product::class,100)->create();
+        factory(App\Model\Review::class,300)->create();
     }
 }
